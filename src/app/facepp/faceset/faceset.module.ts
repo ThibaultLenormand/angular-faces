c7b8abc8111aa8f715956f4b7407fc2e1afd2466
Younes SADMI <younes.sadmi@gmail.com>
2017-06-22 09:22:58 +0200

    faceset remove face service/component

--- src/app/facepp/faceset/faceset.module.ts
+++ src/app/facepp/faceset/faceset.module.ts
@@ -2,11 +2,12 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FacesetCreateComponent } from './component/faceset-create/faceset-create.component';
 import { FacesetAddFaceComponent } from './component/faceset-add-face/faceset-add-face.component';
+import { FacesetRemoveFaceComponent } from './component/faceset-remove-face/faceset-remove-face.component';
 
 @NgModule({
   imports: [
     CommonModule
   ],
-  declarations: [FacesetCreateComponent, FacesetAddFaceComponent]
+  declarations: [FacesetCreateComponent, FacesetAddFaceComponent, FacesetRemoveFaceComponent]
 })
 export class FacesetModule { }
