fbb95dbb5d95409486ce0127ea11d77434e33d09
Younes SADMI <younes.sadmi@gmail.com>
2017-06-22 10:00:06 +0200

    create Face module

--- src/app/facepp/facepp.module.ts
+++ src/app/facepp/facepp.module.ts
@@ -1,6 +1,8 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FacesetModule } from './module/faceset/faceset.module';
+import { FaceModule } from './module/faceset/face.module';
+
 import { DetectComponent } from './component/detect/detect.component';
 import { CompareComponent } from './component/compare/compare.component';
 import { SearchComponent } from './component/search/search.component';
