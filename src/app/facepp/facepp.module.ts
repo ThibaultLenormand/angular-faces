694119c104d53407602ae9ec2f6aaa2d25c0f801
Younes SADMI <younes.sadmi@gmail.com>
2017-06-22 09:21:42 +0200

    faceset add face service/component

--- src/app/facepp/facepp.module.ts
+++ src/app/facepp/facepp.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { FacesetModule } from './faceset/faceset.module';
 
 @NgModule({
     imports: [
--- src/app/facepp/faceset/faceset.module.ts
+++ src/app/facepp/faceset/faceset.module.ts
@@ -1,11 +1,12 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FacesetCreateComponent } from './component/faceset-create/faceset-create.component';
+import { FacesetAddFaceComponent } from './component/faceset-add-face/faceset-add-face.component';
 
 @NgModule({
   imports: [
     CommonModule
   ],
-  declarations: [FacesetCreateComponent]
+  declarations: [FacesetCreateComponent, FacesetAddFaceComponent]
 })
 export class FacesetModule { }
