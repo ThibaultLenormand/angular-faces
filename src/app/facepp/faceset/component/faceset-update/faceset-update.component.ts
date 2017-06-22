e44ce1002f9258f524b3a69abe4e8a1108fad53d
Younes SADMI <younes.sadmi@gmail.com>
2017-06-22 09:24:42 +0200

    faceset update service/component

---
+++ src/app/facepp/faceset/component/faceset-update/faceset-update.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-faceset-update',
+  templateUrl: './faceset-update.component.html',
+  styleUrls: ['./faceset-update.component.css']
+})
+export class FacesetUpdateComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
--- src/app/facepp/faceset/faceset.module.ts
+++ src/app/facepp/faceset/faceset.module.ts
@@ -3,11 +3,12 @@ import { CommonModule } from '@angular/common';
 import { FacesetCreateComponent } from './component/faceset-create/faceset-create.component';
 import { FacesetAddFaceComponent } from './component/faceset-add-face/faceset-add-face.component';
 import { FacesetRemoveFaceComponent } from './component/faceset-remove-face/faceset-remove-face.component';
+import { FacesetUpdateComponent } from './component/faceset-update/faceset-update.component';
 
 @NgModule({
   imports: [
     CommonModule
   ],
-  declarations: [FacesetCreateComponent, FacesetAddFaceComponent, FacesetRemoveFaceComponent]
+  declarations: [FacesetCreateComponent, FacesetAddFaceComponent, FacesetRemoveFaceComponent, FacesetUpdateComponent]
 })
 export class FacesetModule { }
