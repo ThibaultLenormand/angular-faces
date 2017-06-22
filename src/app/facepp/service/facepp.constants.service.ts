e710ad67aacf88f7fe954925f411631811e4506e
Younes SADMI <younes.sadmi@gmail.com>
2017-06-22 09:12:57 +0200

    bug fixed

--- src/app/facepp/service/facepp.constants.service.ts
+++ src/app/facepp/service/facepp.constants.service.ts
@@ -2,7 +2,7 @@ import { Injectable } from '@angular/core';
 
 @Injectable()
 export class Configuration {
-    public const baseUrl: string = 'https://api-us.faceplusplus.com/facepp/v3/';
-    public const detectApi: string = 'detect/';
-    public const fullUrl = this.baseUrl + this.detectApi;
+    public readonly baseUrl: string = 'https://api-us.faceplusplus.com/facepp/v3/';
+    public readonly detectApi: string = 'detect/';
+    public readonly fullUrl = this.baseUrl + this.detectApi;
 }
