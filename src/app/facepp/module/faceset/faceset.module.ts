2f869ac4f2a06ceb6259770725e99d94b2b6d68e
Younes SADMI <younes.sadmi@gmail.com>
2017-06-22 11:41:30 +0200

    bug fixed

--- src/app/facepp/module/faceset/faceset.module.ts
+++ src/app/facepp/module/faceset/faceset.module.ts
@@ -1,5 +1,7 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { RouterModule } from '@angular/router';
+
 import { FacesetCreateComponent } from './component/faceset-create/faceset-create.component';
 import { FacesetAddFaceComponent } from './component/faceset-add-face/faceset-add-face.component';
 import { FacesetRemoveFaceComponent } from './component/faceset-remove-face/faceset-remove-face.component';
@@ -8,9 +10,12 @@ import { FacesetGetDetailComponent } from './component/faceset-get-detail/facese
 import { FacesetDeleteComponent } from './component/faceset-delete/faceset-delete.component';
 import { FacesetGetFaceSetsComponent } from './component/faceset-get-face-sets/faceset-get-face-sets.component';
 
+import { ROUTES } from './faceset.routes';
+
 @NgModule({
   imports: [
-    CommonModule
+    CommonModule,
+    RouterModule.forRoot(ROUTES)
   ],
   declarations: [FacesetCreateComponent, FacesetAddFaceComponent, FacesetRemoveFaceComponent, FacesetUpdateComponent, FacesetGetDetailComponent, FacesetDeleteComponent, FacesetGetFaceSetsComponent]
 })
