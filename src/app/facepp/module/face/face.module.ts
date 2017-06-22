a808f4e58e67d99e5f4e463966993109be0bba4a
Thibault Lenormand <superskoller@gmail.com>
2017-06-22 23:16:40 +0200

    ajax done for face-analyze

--- src/app/facepp/module/face/face.module.ts
+++ src/app/facepp/module/face/face.module.ts
@@ -1,22 +1,34 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterModule } from '@angular/router';
+import { FormsModule } from '@angular/forms';
 
 import { FaceAnalyzeComponent } from './component/face-analyze/face-analyze.component';
 import { FaceGetDetailComponent } from './component/face-get-detail/face-get-detail.component';
 import { FaceSetUserIdComponent } from './component/face-set-user-id/face-set-user-id.component';
 
+
+
 import { ROUTES } from './face.routes';
+import {FaceAnalyzeService} from './service/face-analyze.service';
+import {FaceGetDetailService} from './service/face-get-detail.service';
+import {FaceSetUserIdService} from './service/face-set-user-id.service';
 
 @NgModule({
     imports: [
         CommonModule,
-        RouterModule.forRoot(ROUTES)
+        RouterModule.forRoot(ROUTES),
+        FormsModule
     ],
     declarations: [
         FaceAnalyzeComponent,
         FaceGetDetailComponent,
         FaceSetUserIdComponent
+    ],
+    providers: [
+        FaceAnalyzeService,
+        FaceGetDetailService,
+        FaceSetUserIdService
     ]
 })
 export class FaceModule { }
