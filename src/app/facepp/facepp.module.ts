c8fa7e98f5abe8945c98c313f1965511f5e57d95
Thibault Lenormand <superskoller@gmail.com>
2017-06-22 09:24:51 +0200

    added detect/compare/search services & components

--- src/app/facepp/facepp.module.ts
+++ src/app/facepp/facepp.module.ts
@@ -1,11 +1,14 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { DetectComponent } from './component/detect/detect.component';
+import { CompareComponent } from './component/compare/compare.component';
+import { SearchComponent } from './component/search/search.component';
 
 @NgModule({
     imports: [
         CommonModule
     ],
-    declarations: []
+    declarations: [DetectComponent, CompareComponent, SearchComponent]
 })
 export class FaceppModule {
     
